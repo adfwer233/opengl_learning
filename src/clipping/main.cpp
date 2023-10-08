e51016d5199c39d78c9ffca7b54f63a9ad029b92
adfwer <ba123698745@126.com>
2023-10-08 23:59:49 +0800

    tmp

--- src/clipping/main.cpp
+++ src/clipping/main.cpp
@@ -3,7 +3,7 @@
 #include <common/shader.hxx>
 
 #include <iostream>
-
+#include <array>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -14,6 +14,59 @@ const unsigned int SCR_HEIGHT = 600;
 #ifndef SHADER_DIR
 #define SHADER_DIR "./shader"
 #endif
+
+class VerticesBuffer {
+public:
+	std::array<float, 1000> vertices;
+	int count = 0;
+	VerticesBuffer() = default;
+
+	VerticesBuffer(std::initializer_list<std::initializer_list<float>> init_list) {
+		for (auto list : init_list) {
+			if (list.size() != 3) {
+				std::cout << "ERROR" << std::endl;
+				return;
+			}
+
+			for (int i = 0; auto item : list) {
+				vertices[3 * count + i] = item;
+				i++;
+			}
+
+			count++;
+		}
+	}
+
+	float* get_buffer_address() {
+		return vertices.data();
+	}
+
+	int size() {
+		return vertices.size();
+	}
+
+	void add_vertices(float x, float y, float z) {
+		vertices[count * 3] = x;
+		vertices[count * 3 + 1] = y;
+		vertices[count * 3 + 2] = z;
+		count++;
+	}
+};
+
+VerticesBuffer vertices1{
+	{-0.5f, -0.5f, 0.0f},
+	{0.5f,  -0.5f, 0.0f},
+	{-0.5f,  0.5f, 0.0f},
+	{0.5f,   0.5f, 0.0f},
+};
+
+VerticesBuffer vertices2{
+	{-0.2f, -0.2f, 0.0f},
+	{ 0.2f, -0.2f, 0.0f},
+	{ 0.0f,  0.2f, 0.0f}
+};
+
+unsigned int VBO[2], VAO[2];
 
 int main()
 {
@@ -60,32 +113,20 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		-0.5f,  0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-	};
 
-	float vertices2[] = {
-		-0.2f, -0.2f, 0.0f, // left  
-		 0.2f, -0.2f, 0.0f, // right 
-		 0.0f,  0.2f, 0.0f,
-	};
 
-	unsigned int VBO[2], VAO[2];
 	glGenVertexArrays(2, VAO);
 	glGenBuffers(2, VBO);
 
 	glBindVertexArray(VAO[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices1.count * 4, vertices1.get_buffer_address(), GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices2.count * 4, vertices2.get_buffer_address(), GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -105,7 +146,7 @@ int main()
 		// draw our first triangle
 		glUseProgram(redShader.ID);
 		glBindVertexArray(VAO[0]); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_POINTS, 0, 4);
+		glDrawArrays(GL_POINTS, 0, vertices1.count);
 		glDrawArrays(GL_LINE_STRIP, 0, 4);
 		glBindVertexArray(0);
 
@@ -139,6 +180,13 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS) {
+		vertices1.add_vertices(0.6, 0.6, 0);
+		glBindVertexArray(VAO[0]);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+		glBufferData(GL_ARRAY_BUFFER, vertices1.count * 4, vertices1.get_buffer_address(), GL_STATIC_DRAW);
+	}
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
